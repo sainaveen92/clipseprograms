5bc9e9d95e97441155a9b2698f2ab2732e4eb8b6
sai <sainaveenkota92@gmail.com>
2023-03-31 05:54:00 -0700

    clip

---
+++ sampleProject/src/main/java/sampleProject/Demo.java
@@ -0,0 +1,11 @@
+package sampleProject;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" Hello I am Sai..., this is a test program");
+		System.out.println("this dem class");
+	}
+
+}
